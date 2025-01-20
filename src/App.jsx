e96eeec13f785569ba46aa8dd37ab5ebabf0d3a6
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-20 15:23:02 -0500

    Upload all files

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './App.css'
+import XIcon from './assets/x.svg'
+
+function App() {
+  const [todo, setTodo] = useState([])
+
+  const addTodo = () => {
+    const input = document.getElementById('todo');
+    let inputValue = input.value;
+    if (inputValue) {
+      setTodo((prev) => [
+        ...prev,
+        {ID: todo.length, value: inputValue},
+      ]);
+    }
+    input.value = ''
+  }
+  const remove = index => {
+    const newIndex = [...todo];
+    newIndex.splice(index, 1)
+    setTodo(newIndex);
+  }
+  return (
+    <>
+    <div className="container">
+      <section className="todo-list">
+        <h1>To-Do List</h1>
+      </section>
+      <section className="add-todo">
+        <input type="text" placeholder='Make dinner...' id="todo"/>
+        <button onClick={addTodo}>Add</button>
+      </section>
+      <section className="list">
+        <ul>
+          {todo.map((element, i) => {
+            return (<>
+            <div>
+            <li key={element.ID + 1}>{element.value}</li>
+            <button onClick={() => remove(i)}><img src={XIcon} alt="x button" /></button>
+            </div>
+            </>)
+          })}
+        </ul>
+      </section>
+    </div>
+    <div className="attribute">
+      <p>Project by <a href="https://github.com/JunLovin" target="_blank">JunLovin</a></p>
+    </div>
+    </>
+  )
+}
+
+export default App
